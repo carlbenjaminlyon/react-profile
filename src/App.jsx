1a6777720665670e0d60727c0b14a669b6c6c744
Ben Lyon <carlbenjaminlyon@gmail.com>
2025-02-16 14:14:24 -0600

    eslint config

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
+function App() {
+
+    return (
+        <div>
+        <h1>Hello!</h1>
+        </div>
+    );
+};
