42e78fd7164be3cbb977b51e66f37ce9981b036c
Ben Lyon <carlbenjaminlyon@gmail.com>
2025-02-16 15:11:40 -0600

    profile component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './styles/App.css';
 import ButtonTest from './assets/components/Button';
+import Profile from './assets/components/Profile';
 
 
 function App() {
 
 	return (
 		<div>
+			<Profile />
 			<ButtonTest />
 		</div>
 	);
---
+++ src/assets/components/Profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Profile() {
+    return (
+        <img
+            src="https://avatars.githubusercontent.com/u/83849531?v=4"
+            alt="Ben"
+        />
+    )
+};
+export default Profile;
