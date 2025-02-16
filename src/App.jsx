f3cc77260a6cf3eeb224d2ec78ea582fc6930564
Ben Lyon <carlbenjaminlyon@gmail.com>
2025-02-16 14:31:12 -0600

    adding basic page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './styles/App.css';
+import ButtonTest from './assets/components/Button';
 
 
 function App() {
 
     return (
         <div>
-        <h1>Hello!</h1>
+            <ButtonTest />
         </div>
     );
 };
+
+export default App;
---
+++ src/assets/components/Button.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Button} from '@mui/material';
+
+function ButtonTest() {
+    return <Button variant="contained">Hello World!</Button>;
+}
+
+export default ButtonTest;
