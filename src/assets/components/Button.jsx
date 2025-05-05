3b9deb20270234e14d7a9d8c5d8178d525b28fed
Ben Lyon <carlbenjaminlyon@gmail.com>
2025-05-04 22:23:20 -0500

    toss in trash

--- src/assets/components/Button.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {Button} from '@mui/material';
-
-function ButtonTest() {
-    return <Button variant="contained">Hello World!</Button>;
-}
-
-export default ButtonTest;
